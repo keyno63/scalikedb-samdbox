04b5056f177f324110806b79f2cbc08a293cff57
Keyno <kofujiw@gmail.com>
2020-01-01 23:23:58 +0900

    move dbmodule from build.sbt to Dependency.sbt

--- project/Dependency.scala
+++ project/Dependency.scala
@@ -6,4 +6,18 @@ object Dependency {
   val postgresVersion = "42.2.8"
   val mysqlVersion = "8.0.18"
 
+  val dbModule: Seq[ModuleID] = Seq(
+    "org.scalikejdbc"        %% "scalikejdbc",
+    "org.scalikejdbc"        %% "scalikejdbc-config",
+    "org.scalikejdbc"        %% "scalikejdbc-jsr310"
+  ).map(_ % scalikejdbcVersion) ++ Seq(
+    "org.scalikejdbc"        %% "scalikejdbc-play-initializer" % "2.7.1-scalikejdbc-3.3",
+    "org.scalatestplus.play" %% "scalatestplus-play"           % "4.0.1" % Test,
+    "com.h2database"         %  "h2"                           % "1.4.192",
+    // https://mvnrepository.com/artifact/org.postgresql/postgresql
+    "org.postgresql" % "postgresql" % postgresVersion,
+    // https://mvnrepository.com/artifact/mysql/mysql-connector-java
+    "mysql" % "mysql-connector-java" % mysqlVersion
+  )
+
 }
