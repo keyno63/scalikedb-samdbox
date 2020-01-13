4b9d53250a1519c1c97b2cb33545231be5151714
Keyno <kofujiw@gmail.com>
2020-01-13 23:49:19 +0900

    Post object.

--- app/repository/SampleRepository.scala
+++ app/repository/SampleRepository.scala
@@ -1,8 +1,5 @@
 package repository
 
-import java.time.OffsetDateTime
-
-import play.api.libs.json.{Json, Writes}
 import scalikejdbc.NamedDB
 import scalikejdbc._
 import scalikejdbc.jsr310._
@@ -21,14 +18,4 @@ object SampleRepository {
   def add(post: Post): Unit = NamedDB('pgsql) localTx { implicit session =>
     sql"INSERT INTO posts (body, date) VALUES (${post.body}, ${post.date})".update().apply()
   }
-}
-
-case class Post(id: Long, body: String, date: OffsetDateTime)
-
-object Post {
-
-  implicit val writes: Writes[Post] = Json.writes[Post]
-
-  def apply(body: String, date: OffsetDateTime): Post =
-    Post(0, body, date)
 }
