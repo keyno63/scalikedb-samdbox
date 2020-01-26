3c650a813189a684fe957ef3ecbb54d11142db6d
Keyno <kofujiw@gmail.com>
2020-01-26 23:31:21 +0900

    update sample builder, use foldRight.

--- app/domain/Domain.scala
+++ app/domain/Domain.scala
@@ -12,6 +12,17 @@ object Domain extends App {
     "X!!!"
   )
   x.foldLeft(sample){_.seq(_)}.build()
+
+  val sample2 = DomainBuilder()
+    .name("right")
+    .id(3)
+  val y = Seq(
+    "number 2",
+    "sequence",
+    "sample"
+  )
+  y.foldRight(sample2)((yy, ss) => ss.seq(yy)).build()
+
 }
 
 case class DomainBase(name: String, id: Int, sq: Seq[String])
