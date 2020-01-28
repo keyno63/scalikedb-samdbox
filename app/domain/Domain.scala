f51af5bcb4e1f59a402d6502c7722a8af9732137
Keyno <kofujiw@gmail.com>
2020-01-28 23:58:33 +0900

    update foldLeft/Right.

--- app/domain/Domain.scala
+++ app/domain/Domain.scala
@@ -23,6 +23,15 @@ object Domain extends App {
   )
   seq2.foldRight(sample2)((yy, ss) => ss.seq(yy)).build()
 
+  val sample3 = DomainBuilder()
+    .name("right")
+    .id(4)
+  val seq3 = Seq(
+    "number 3",
+    "sequence",
+    "sample"
+  )
+  seq2.foldRight(sample2)((yy, ss) => ss.seq(yy)).build()
 }
 
 case class DomainBase(name: String, id: Int, sq: Seq[String])
