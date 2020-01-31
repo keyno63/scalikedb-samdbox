2e6c2eaeeaf68c613052afe9f863c2a7a23fa13e
Keyno <kofujiw@gmail.com>
2020-01-31 23:58:57 +0900

    add sample type safe builder.

--- app/domain/SampleTypeSafeBuilder.scala
+++ app/domain/SampleTypeSafeBuilder.scala
@@ -1,5 +1,13 @@
 package domain
 
-object SampleTypeSafeBuilder {
+case class SampleData[Base](ingredients: Map[String, Int])
 
+sealed trait Base
+trait On extends Base
+trait Off extends Base
+
+type NotMocoRecipe = SampleData[On]
+type MocosRecipe = SampleData[Off]
+
+object SampleTypeSafeBuilder {
 }
