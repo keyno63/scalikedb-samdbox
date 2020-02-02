3e2a9a33da8fc9a68124d71154becfdae807c825
Keyno <kofujiw@gmail.com>
2020-02-02 23:45:11 +0900

    fix

--- app/domain/SampleTypeSafeBuilder.scala
+++ app/domain/SampleTypeSafeBuilder.scala
@@ -3,11 +3,8 @@ package domain
 case class SampleData[Base](ingredients: Map[String, Int])
 
 sealed trait Base
-trait On extends Base
-trait Off extends Base
-
-type OnSampleData = SampleData[On]
-type OffSampleData = SampleData[Off]
+private trait On extends Base
+private trait Off extends Base
 
 object SampleTypeSafeBuilder {
   def apply(): SampleTypeSafeBuilder[Off] = new SampleTypeSafeBuilder()
